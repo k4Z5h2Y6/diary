87beb9dd7a337239082de5545e2a7961faf648fa
k4Z5h2Y6 <k8bi69x4@gmail.com>
2024-03-15 14:30:07 +0900

    /account,/profile削除

--- src/consts/database.types.ts
+++ src/consts/database.types.ts
@@ -14,25 +14,16 @@ export interface Database {
           id: string
           updated_at: string | null
           username: string | null
-          full_name: string | null
-          avatar_url: string | null
-          website: string | null
         }
         Insert: {
           id: string
           updated_at?: string | null
           username?: string | null
-          full_name?: string | null
-          avatar_url?: string | null
-          website?: string | null
         }
         Update: {
           id?: string
           updated_at?: string | null
           username?: string | null
-          full_name?: string | null
-          avatar_url?: string | null
-          website?: string | null
         }
       }
     }

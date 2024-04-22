8b5a2c6ac11718f91bf03dad0979e4d9708ddecd
k4Z5h2Y6 <k8bi69x4@gmail.com>
2024-04-22 18:42:25 +0900

    サインアップテスト

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,34 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import { useState } from "react";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { AuthForm } from "@/components/authForm";
+
+// export default function SignUp(){
+//   return (
+//     <>
+//       <AuthForm/>
+//     </>
+//   )
+// }
+
 import { Button, TextField } from "@mui/material";
 import Link from "next/link";
+import { login, signup } from "../auth/login/actions";
+import { redirect } from "next/dist/server/api-utils";
 
 export default function Login() {
   return (
     <>
+      {/* <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" name="email" type="email" required />
+        <label htmlFor="password">Password:</label>
+        <input id="password" name="password" type="password" required />
+        <button formAction={redirect("/auth/login")}>Log in</button>
+        <button formAction={signup}>Sign up</button>
+      </form> */}
       <form action="/auth/login" method="post">
         <TextField
           name="email"
